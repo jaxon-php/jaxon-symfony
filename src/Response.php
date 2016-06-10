c75e7f91a51b0a5a4df8e27a8e8125a2057fa646
feuzeu <thierry.feuzeu@gmail.com>
2016-06-10 16:01:40 -0400

    Added composer.json file and src dir.

---
+++ src/Response.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Jaxon\Symfony;
+
+use Symfony\Component\HttpFoundation\Response as HttpResponse;
+
+class Response extends \Jaxon\Response\Response
+{
+    /**
+     * Create a new Response instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Wrap the Jaxon response in a Symfony HTTP response.
+     *
+     * @param  string  $code
+     *
+     * @return string  the HTTP response
+     */
+    public function http($code = '200')
+    {
+        $response = new HttpResponse();
+        $response->headers->set('Content-Type', $this->getContentType());
+        $response->setCharset($this->getCharacterEncoding());
+        $response->setStatusCode($code);
+        $response->setContent($this->getOutput());
+        // prints the HTTP headers followed by the content
+        $response->send();
+    }
+}
---
+++ src/View.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Jaxon\Symfony;
+
+class View
+{
+    protected static $data;
+    protected $controller;
+
+    public function __construct($controller)
+    {
+        if(!is_array(self::$data))
+        {
+            self::$data = array();
+        }
+        $this->controller = $controller;
+    }
+
+    /**
+     * Make a piece of data available for all views
+     *
+     * @param string        $name            The data name
+     * @param string        $value           The data value
+     * 
+     * @return void
+     */
+    public function share($name, $value)
+    {
+        self::$data[$name] = $value;
+    }
+
+    /**
+     * Render a template
+     *
+     * @param string        $template        The template path
+     * @param string        $data            The template data
+     * 
+     * @return mixed        The rendered template
+     */
+    public function render($template, array $data = array())
+    {
+        return $this->controller->render($template, array_merge(self::$data, $data), true);
+    }
+}
