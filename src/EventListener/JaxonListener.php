d553dcb958334e724f4ba62af69187332e936ee3
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2025-06-28 00:42:39 +0200

    The listener is called only once.

--- src/EventListener/JaxonListener.php
+++ src/EventListener/JaxonListener.php
@@ -7,11 +7,25 @@ use Symfony\Component\HttpKernel\Event\ControllerEvent;
 
 class JaxonListener
 {
+    /**
+     * @var bool
+     */
+    private bool $alreadyCalled = false;
+
+    /**
+     * @param Jaxon $jaxon
+     */
     public function __construct(private Jaxon $jaxon)
     {}
 
     public function onKernelController(ControllerEvent $event)
     {
+        if($this->alreadyCalled)
+        {
+            return;
+        }
+
+        $this->alreadyCalled = true;
         $this->jaxon->setup();
     }
 }
